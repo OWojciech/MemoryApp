47ab41cf74862f81a8d9f0b50a62e3d903be9024
Wojtek <olejarzwojtek@gmail.com>
2019-03-23 12:03:15 +0100

    Pierwszy commit

---
+++ app/src/main/java/com/example/prm_projekt1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prm_projekt1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
