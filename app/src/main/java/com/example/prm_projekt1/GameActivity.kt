57a32d4770ffee0103cbde3289b67a98befdbc6e
Wojtek <olejarzwojtek@gmail.com>
2019-03-25 13:36:39 +0100

    Wypełnienie guzików charami i odkrywanie po kliknięciu guzika

--- app/src/main/java/com/example/prm_projekt1/GameActivity.kt
+++ app/src/main/java/com/example/prm_projekt1/GameActivity.kt
@@ -11,7 +11,8 @@ import kotlinx.android.synthetic.main.activity_game.*
 class GameActivity() : AppCompatActivity() {
 
     val tableLayout by lazy { TableLayout(this) }
-
+    val memoryTable by lazy { ArrayList<Char>()}
+    var clickCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +23,33 @@ class GameActivity() : AppCompatActivity() {
         }
         var rows = intent.getIntExtra("rows", 5)
         var columns = intent.getIntExtra("columns", 5)
+        initMemTable()
+        shuffleTable(rows*columns)
         createTable(rows, columns)
+
+    }
+    private fun initMemTable(){
+        var ascii = 97
+        var tmp = 20
+        while(tmp > 0){
+            memoryTable.add(ascii.toChar())
+            memoryTable.add(ascii.toChar())
+            ascii++
+            tmp--
+        }
     }
 
+    private fun shuffleTable(count: Int){
+        var tmp = count
+        while(tmp > 0){
+            val i = Math.floor(Math.random() * memoryTable.size)
+            val j = Math.floor(Math.random() * memoryTable.size)
+            var temp = memoryTable.get(j.toInt())
+            memoryTable.set(j.toInt(), memoryTable.get(i.toInt()))
+            memoryTable.set(i.toInt(), temp)
+            tmp--
+        }
+    }
     private fun createTable(rows: Int, cols: Int) {
 
         for (i in 0 until rows) {
@@ -37,7 +62,9 @@ class GameActivity() : AppCompatActivity() {
                 row.addView(Button(this).apply {
                     layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
                         TableRow.LayoutParams.WRAP_CONTENT)
-                    text = "R $i C $j"
+                    setOnClickListener{
+                        text = memoryTable.get((i*cols +j)%memoryTable.size).toString()
+                    }
                 })
             }
             tableLayout.addView(row)
